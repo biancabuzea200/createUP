74d8c76581aecde72d8005d195a94f56cc186696
biancabuzea200 <bianca@lukso.io>
2023-11-01 10:40:54 +0100

    update web3 files

--- src/deployUP.js
+++ src/deployUP.js
@@ -1,21 +1,25 @@
-import { LSPFactory } from '@lukso/lsp-factory.js';
+// You might need to use node v16.0.0 (nvm use 16.0.0) when running this
+// This is because node made a change and a flag is required in the fetch command, and the ipfs-http-client hasn't made an update
+// So only older versions of node will work.
+// You can do this with `nvm use 16.0.0`
+
+
 import Web3 from 'web3';
 const web3 = new Web3("https://rpc.testnet.lukso.network");
+import { LSPFactory } from '@lukso/lsp-factory.js';
 import dotenv from 'dotenv/config';
 
 const PRIVATE_KEY = process.env.MY_PRIVATE_KEY;
 const myEOA = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
 
-// Initialize the LSPFactory with the Testnet RPC endpoint and your EOA's private key, which will deploy the UP smart contracts
 const lspFactory = new LSPFactory('https://rpc.testnet.lukso.network/', {
     deployKey: PRIVATE_KEY,
-    chainId: 4201,
+    chainId: 4201, // LUKSO Testnet
 });
 
-// Deploy our Universal Profile
 async function createUniversalProfile() {
     const deployedContracts = await lspFactory.UniversalProfile.deploy({
-        controllerAddresses: [myEOA.address], // our EOA that will be controlling the UP
+        controllerAddresses: [myEOA.address],
         lsp3Profile: {
             name: 'My Universal Profile',
             description: 'My Cool Universal Profile',
@@ -32,7 +36,8 @@ async function createUniversalProfile() {
     const myUPAddress = deployedContracts.LSP0ERC725Account.address;
     console.log('my Universal Profile address: ', myUPAddress);
 
+    // Now we can add this UP address to our .env file
+
     return deployedContracts;
 }
-
 createUniversalProfile();
--- src/readProfileData.js
+++ src/readProfileData.js
@@ -1,11 +1,11 @@
 // Import and Network Setup
-//0x2875d1733346bF4618485a91F2D9E5FB544f4fc5
+
 import Web3 from 'web3';
 import { ERC725 } from '@erc725/erc725.js';
 import lsp3ProfileSchema from '@erc725/erc725.js/schemas/LSP3ProfileMetadata.json' assert { type: 'json' };
 
 // Our static variables
-//const SAMPLE_PROFILE_ADDRESS = '0x6979474Ecb890a8EFE37daB2b9b66b32127237f7';
+//const SAMPLE_PROFILE_ADDRESS2 = '0x6979474Ecb890a8EFE37daB2b9b66b32127237f7';
 const SAMPLE_PROFILE_ADDRESS = "0xB031363560403179Aac100d51864e27fFF4D7807";
 const RPC_ENDPOINT = 'https://rpc.testnet.lukso.network';
 const IPFS_GATEWAY = 'https://api.universalprofile.cloud/ipfs';
